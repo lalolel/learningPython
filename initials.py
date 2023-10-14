82c47611bbed0f2ad60433c7d128c39cd579965d
Lalo <114935993+lalolel@users.noreply.github.com>
2023-10-14 23:35:08 +0300

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,36 @@
+print("""L             L    
+L             L
+L             L
+L             L
+L             L
+LLLLLL        LLLLLL""")
+#Lalo 
+# I love coding
+
+name = """
+L             L    
+L             L
+L             L
+L             L
+L             L
+LLLLLL        LLLLLL
+"""
+print(name)
+
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
